7db81430e12752da6ba2262c396031988adc24c3
JakobEvans <evansja@sonoma.edu>
2021-09-24 19:58:14 -0700

    finalize Overriding practice

---
+++ src/com/cognixia/jump/comparablePractice/OverridingRunner.java
@@ -0,0 +1,26 @@
+// Jakob Evans
+// 9/15/21
+// Method Overriding
+
+package com.cognixia.jump.comparablePractice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OverridingRunner {
+	public static void main(String[] args) throws Throwable {
+			
+			
+		
+			
+			Square sq = new Square("red", 5);
+			
+			sq.printShapeData();
+			
+			sq.finalize();
+			
+		
+//			
+		}
+}
